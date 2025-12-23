b9e4c6d5e356961740098dc31a3592cad875cc30
Thalita Andrade <thalita.andrade21@gmail.com>
2025-12-22 23:12:43 -0300

    add teste proxy

--- infra/routes.tf
+++ infra/routes.tf
@@ -27,4 +27,11 @@ module "api_gateway_categories_routes" {
   gwapi_route_key = "ANY /v1/categories/{proxy+}"
   alb_proxy_id    = aws_apigatewayv2_integration.alb_proxy.id
 }
+module "api_gateway_v1_proxy_route" {
+  source     = "git::https://github.com/FIAP-11soat-grupo-21/infra-core.git//modules/API-Gateway-Routes?ref=main"
+  depends_on = [module.catalog_api]
+  api_id          = data.terraform_remote_state.infra.outputs.api_gateway_id
+  gwapi_route_key = "ANY /v1/{proxy+}"
+  alb_proxy_id    = aws_apigatewayv2_integration.alb_proxy.id
+}
 
