c1944d2b45b7da6257cbc0105f469fa293d7c2a9
Thalita Andrade <thalita.andrade21@gmail.com>
2025-12-22 21:41:10 -0300

    add rota health no gateway

--- infra/routes.tf
+++ infra/routes.tf
@@ -15,3 +15,11 @@ module "api_gateway_categories_routes" {
   gwapi_route_key = "ANY /categories/{proxy+}"
   alb_proxy_id    = aws_apigatewayv2_integration.alb_proxy.id
 }
+module "api_gateway_health_route" {
+  source     = "git::https://github.com/FIAP-11soat-grupo-21/infra-core.git//modules/API-Gateway-Routes?ref=main"
+  depends_on = [module.catalog_api]
+
+  api_id          = data.terraform_remote_state.infra.outputs.api_gateway_id
+  gwapi_route_key = "GET /health"
+  alb_proxy_id    = aws_apigatewayv2_integration.alb_proxy.id
+}
