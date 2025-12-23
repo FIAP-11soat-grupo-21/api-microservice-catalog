4fa1de75d2a16dfb394f3e3ee12ba9c1a93a34eb
Thalita Andrade <thalita.andrade21@gmail.com>
2025-12-23 12:55:04 -0300

    fix formatacao

--- infra/routes.tf
+++ infra/routes.tf
@@ -30,8 +30,8 @@ module "api_gateway_health_route" {
 #}
 
 module "api_gateway_v1_proxy_route" {
-  source     = "git::https://github.com/FIAP-11soat-grupo-21/infra-core.git//modules/API-Gateway-Routes?ref=main"
-  depends_on = [module.catalog_api]
+  source          = "git::https://github.com/FIAP-11soat-grupo-21/infra-core.git//modules/API-Gateway-Routes?ref=main"
+  depends_on      = [module.catalog_api]
   api_id          = data.terraform_remote_state.infra.outputs.api_gateway_id
   gwapi_route_key = "ANY /v1/{proxy+}"
   alb_proxy_id    = aws_apigatewayv2_integration.alb_proxy.id
