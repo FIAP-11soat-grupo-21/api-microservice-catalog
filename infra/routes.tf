1035a2a0b31adf2865a23dd555cd70266dc23dfc
Thalita Andrade <thalita.andrade21@gmail.com>
2025-12-22 22:43:13 -0300

    ajuste rotas API Gateway

--- infra/routes.tf
+++ infra/routes.tf
@@ -1,25 +1,39 @@
 module "api_gateway_products_routes" {
   source     = "git::https://github.com/FIAP-11soat-grupo-21/infra-core.git//modules/API-Gateway-Routes?ref=main"
   depends_on = [module.catalog_api]
-
   api_id          = data.terraform_remote_state.infra.outputs.api_gateway_id
+  vpc_link_id       = data.terraform_remote_state.infra.outputs.api_gateway_vpc_link_id
+  alb_listener_arn  = module.ALB.listener_arn
   gwapi_route_key = "ANY /products/{proxy+}"
-  alb_proxy_id    = aws_apigatewayv2_integration.alb_proxy.id
+  # alb_proxy_id    = aws_apigatewayv2_integration.alb_proxy.id
 }
 
 module "api_gateway_categories_routes" {
   source     = "git::https://github.com/FIAP-11soat-grupo-21/infra-core.git//modules/API-Gateway-Routes?ref=main"
   depends_on = [module.catalog_api]
-
   api_id          = data.terraform_remote_state.infra.outputs.api_gateway_id
+  vpc_link_id       = data.terraform_remote_state.infra.outputs.api_gateway_vpc_link_id
+  alb_listener_arn  = module.ALB.listener_arn  
   gwapi_route_key = "ANY /categories/{proxy+}"
-  alb_proxy_id    = aws_apigatewayv2_integration.alb_proxy.id
+  # alb_proxy_id    = aws_apigatewayv2_integration.alb_proxy.id
 }
 module "api_gateway_health_route" {
   source     = "git::https://github.com/FIAP-11soat-grupo-21/infra-core.git//modules/API-Gateway-Routes?ref=main"
   depends_on = [module.catalog_api]
-
   api_id          = data.terraform_remote_state.infra.outputs.api_gateway_id
+  vpc_link_id       = data.terraform_remote_state.infra.outputs.api_gateway_vpc_link_id
+  alb_listener_arn  = module.ALB.listener_arn  
   gwapi_route_key = "GET /health"
-  alb_proxy_id    = aws_apigatewayv2_integration.alb_proxy.id
+  # alb_proxy_id    = aws_apigatewayv2_integration.alb_proxy.id
 }
+
+
+# Health Funcional:
+# module "api_gateway_health_route" {
+#   source     = "git::https://github.com/FIAP-11soat-grupo-21/infra-core.git//modules/API-Gateway-Routes?ref=main"
+#   depends_on = [module.catalog_api]
+
+#   api_id          = data.terraform_remote_state.infra.outputs.api_gateway_id
+#   gwapi_route_key = "GET /health"
+#   alb_proxy_id    = aws_apigatewayv2_integration.alb_proxy.id
+# }
