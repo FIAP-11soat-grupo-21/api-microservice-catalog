b2fa312e2dab54a6fc640b0d055d24847fc4b90e
Thalita Andrade <thalita.andrade21@gmail.com>
2025-12-23 23:28:00 -0300

    ajustes rotas gw

---
+++ infra/route-3.tf
@@ -0,0 +1,26 @@
+module "GetCatalogAPIRoute" {
+  source     = "git::https://github.com/FIAP-11soat-grupo-21/infra-core.git//modules/API-Gateway-Routes?ref=main"
+  depends_on = [module.catalog_api]
+
+  api_id          = data.terraform_remote_state.infra.outputs.api_gateway_id
+  gwapi_route_key = "GET /categories/{proxy+}"
+  alb_proxy_id    = aws_apigatewayv2_integration.alb_proxy.id
+}
+
+module "PostCatalogAPIRoute" {
+  source     = "git::https://github.com/FIAP-11soat-grupo-21/infra-core.git//modules/API-Gateway-Routes?ref=main"
+  depends_on = [module.catalog_api]
+
+  api_id          = data.terraform_remote_state.infra.outputs.api_gateway_id
+  gwapi_route_key = "POST /categories/{proxy+}"
+  alb_proxy_id    = aws_apigatewayv2_integration.alb_proxy.id
+}
+
+module "api_gateway_health_route" {
+  source     = "git::https://github.com/FIAP-11soat-grupo-21/infra-core.git//modules/API-Gateway-Routes?ref=main"
+  depends_on = [module.catalog_api]
+
+  api_id          = data.terraform_remote_state.infra.outputs.api_gateway_id
+  gwapi_route_key = "GET /health"
+  alb_proxy_id    = aws_apigatewayv2_integration.alb_proxy.id
+}
--- infra/routes-2.tf
+++ infra/routes-2.tf
@@ -1,28 +1,28 @@
-module "api_gateway_categories_routes" {
-  source     = "git::https://github.com/FIAP-11soat-grupo-21/infra-core.git//modules/API-Gateway-Routes?ref=main"
-  depends_on = [module.catalog_api]
+# module "api_gateway_categories_routes" {
+#   source     = "git::https://github.com/FIAP-11soat-grupo-21/infra-core.git//modules/API-Gateway-Routes?ref=main"
+#   depends_on = [module.catalog_api]
 
-  api_id          = data.terraform_remote_state.infra.outputs.api_gateway_id
-  gwapi_route_key = "ANY /categories/{proxy+}"
-  alb_proxy_id    = aws_apigatewayv2_integration.alb_proxy.id
-}
+#   api_id          = data.terraform_remote_state.infra.outputs.api_gateway_id
+#   gwapi_route_key = "ANY /categories/{proxy+}"
+#   alb_proxy_id    = aws_apigatewayv2_integration.alb_proxy.id
+# }
 
-module "api_gateway_products_routes" {
-  source     = "git::https://github.com/FIAP-11soat-grupo-21/infra-core.git//modules/API-Gateway-Routes?ref=main"
-  depends_on = [module.catalog_api]
+# module "api_gateway_products_routes" {
+#   source     = "git::https://github.com/FIAP-11soat-grupo-21/infra-core.git//modules/API-Gateway-Routes?ref=main"
+#   depends_on = [module.catalog_api]
 
-  api_id          = data.terraform_remote_state.infra.outputs.api_gateway_id
-  gwapi_route_key = "ANY /products/{proxy+}"
+#   api_id          = data.terraform_remote_state.infra.outputs.api_gateway_id
+#   gwapi_route_key = "ANY /products/{proxy+}"
 
-  alb_proxy_id = aws_apigatewayv2_integration.alb_proxy.id
-}
+#   alb_proxy_id = aws_apigatewayv2_integration.alb_proxy.id
+# }
 
-module "api_gateway_health_route" {
-  source     = "git::https://github.com/FIAP-11soat-grupo-21/infra-core.git//modules/API-Gateway-Routes?ref=main"
-  depends_on = [module.catalog_api]
+# module "api_gateway_health_route" {
+#   source     = "git::https://github.com/FIAP-11soat-grupo-21/infra-core.git//modules/API-Gateway-Routes?ref=main"
+#   depends_on = [module.catalog_api]
 
-  api_id          = data.terraform_remote_state.infra.outputs.api_gateway_id
-  gwapi_route_key = "GET /health"
-  alb_proxy_id    = aws_apigatewayv2_integration.alb_proxy.id
-}
+#   api_id          = data.terraform_remote_state.infra.outputs.api_gateway_id
+#   gwapi_route_key = "GET /health"
+#   alb_proxy_id    = aws_apigatewayv2_integration.alb_proxy.id
+# }
 
