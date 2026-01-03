e24a07f3866dfc34382ba8b838a73c0635fe78ca
Thalita Andrade <thalita.andrade21@gmail.com>
2026-01-03 10:43:41 -0300

    corrige modulo API

--- infra/main-2.tf
+++ infra/main-2.tf
@@ -37,7 +37,7 @@ module "catalog_api" {
 
 module "GetCatalogAPIRoute" {
   source     = "git::https://github.com/FIAP-11soat-grupo-21/infra-core.git//modules/API-Gateway-Routes?ref=main"
-  depends_on = [module.customer_api, ]
+  depends_on = [module.catalog_api, ]
 
   api_id       = data.terraform_remote_state.infra.outputs.api_gateway_id
   alb_proxy_id = aws_apigatewayv2_integration.alb_proxy.id
