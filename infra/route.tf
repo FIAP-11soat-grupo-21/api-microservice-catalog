0e13fa4a6efd6720484707a753942af92c8106b9
Thalita Andrade <thalita.andrade21@gmail.com>
2026-01-03 10:27:42 -0300

    remove duplicidade

--- infra/route.tf
+++ infra/route.tf
@@ -1,10 +1,10 @@
-module "GetCatalogAPIRoute" {
-  source     = "git::https://github.com/FIAP-11soat-grupo-21/infra-core.git//modules/API-Gateway-Routes?ref=main"
-  depends_on = [module.catalog_api]
+# module "GetCatalogAPIRoute" {
+#   source     = "git::https://github.com/FIAP-11soat-grupo-21/infra-core.git//modules/API-Gateway-Routes?ref=main"
+#   depends_on = [module.catalog_api]
 
-  api_id       = data.terraform_remote_state.infra.outputs.api_gateway_id
-  alb_proxy_id = aws_apigatewayv2_integration.alb_proxy.id
+#   api_id       = data.terraform_remote_state.infra.outputs.api_gateway_id
+#   alb_proxy_id = aws_apigatewayv2_integration.alb_proxy.id
 
-  endpoints = var.api_endpoints
-}
+#   endpoints = var.api_endpoints
+# }
 
