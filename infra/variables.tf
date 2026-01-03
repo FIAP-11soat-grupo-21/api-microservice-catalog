e4d88edaeeb029def1c39a38bc3cf9e27036681d
Thalita Andrade <thalita.andrade21@gmail.com>
2026-01-03 11:16:16 -0300

    corrige variaveis

--- infra/variables.tf
+++ infra/variables.tf
@@ -51,50 +51,6 @@ variable "api_endpoints" {
     auth_integration_id = optional(string)
   }))
 }
-variable "application_name" {
-  description = "Nome da aplicação ECS"
-  type        = string
-}
-
-variable "image_name" {
-  description = "Nome da imagem do container"
-  type        = string
-}
-
-variable "image_port" {
-  description = "Porta do container"
-  type        = number
-}
-
-variable "desired_count" {
-  description = "Número desejado de tarefas ECS"
-  type        = number
-  default     = 1
-}
-
-variable "container_environment_variables" {
-  description = "Variáveis de ambiente do container"
-  type        = map(string)
-  default     = {}
-}
-
-variable "container_secrets" {
-  description = "Segredos do container"
-  type        = map(string)
-  default     = {}
-}
-
-variable "health_check_path" {
-  description = "Caminho de verificação de integridade do serviço"
-  type        = string
-  default     = "/health"
-}
-
-variable "task_role_policy_arns" {
-  description = "Lista de ARNs de políticas para anexar à função da tarefa ECS"
-  type        = list(string)
-  default     = []
-}
 
 variable "alb_is_internal" {
   description = "Se o ALB é interno"
